f62e8ff06abd78ed19cfbbddaccdb97ada1dadcb
Luis Villalta <luis.villaltar@salud.gob.sv>
2022-09-16 13:26:47 -0600

    WIP: Datatable

--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -1,15 +1,34 @@
 import React from 'react'
-import { Breadcrumbs, Container, Link, Paper, Typography } from '@mui/material'
 import { MainLayout } from '../../layouts/MainLayout'
 import { BreadcrumbItem, Heading } from '../../components/Heading'
+import { Box } from '@mui/material'
+import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+
+const items: BreadcrumbItem[] = [{ title: 'Users', current: true }]
+
+const columns: GridColDef[] = [
+  { field: 'id', headerName: 'ID', width: 70 },
+  { field: 'firstName', headerName: 'First name', width: 130 },
+  { field: 'lastName', headerName: 'Last name', width: 130 },
+  { field: 'email', headerName: 'Email', width: 130 },
+];
 
 export const Users = () => {
-  const items: BreadcrumbItem[] = [
-    { title: 'Users', current: true },
-  ]
   return (
     <MainLayout>
       <Heading title='Users' items={items} />
+
+      <Box sx={{ p: 6 }}>
+        <div style={{ height: 400, width: '100%' }}>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            pageSize={5}
+            rowsPerPageOptions={[5]}
+            checkboxSelection
+          />
+        </div>
+      </Box>
     </MainLayout>
   )
 }
